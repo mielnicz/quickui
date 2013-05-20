45d644beef12be61e78f3c4a2dea9af8fba06a5a
Shane Gough <shane@thegaragelab.com>
2013-05-20 20:56:36 +1000

    Added a simple driver test.

---
+++ testing/simple/quicktest.c
@@ -0,0 +1,16 @@
+/*--------------------------------------------------------------------------*
+* Simple test program for the quickgfx library.
+*---------------------------------------------------------------------------*
+* 20-May-2013 ShaneG
+*
+* This is a simple test program to exercise the driver (just to ensure that
+* graphics are being sent to the output device). All it does is draw random
+* rectangles on the screen until it is killed.
+*--------------------------------------------------------------------------*/
+#include <quickgfx.h>
+
+/** Program entry point
+ */
+int main(int argc, char *argv[]) {
+  return 0;
+  }
