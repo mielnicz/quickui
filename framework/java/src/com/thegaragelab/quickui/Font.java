a8189aa49ad9f1ed63b5c648cb8b120781ce53d6
shane <shane@thegaragelab.com>
2013-06-01 23:42:40 +1000

    Added the 'Font' asset type.

---
+++ framework/java/src/com/thegaragelab/quickui/Font.java
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------*
+* $Id$
+*----------------------------------------------------------------------------*
+* 01/06/2013 - shane
+* 
+* Initial version
+*---------------------------------------------------------------------------*/
+package com.thegaragelab.quickui;
+
+/** Represents a font
+ *
+ */
+public class Font extends Asset {
+  //-------------------------------------------------------------------------
+  // Construction and initialisation
+  //-------------------------------------------------------------------------
+  
+  /** Constructor
+   * 
+   * @param data the raw data for the asset.
+   * @param size the number of bytes in the data array for the asset.
+   */
+  Font(byte[] data, int size) {
+    super();
+    }
+
+  }
