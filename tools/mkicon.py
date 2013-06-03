d1b7c7277bdc656738432141ff887cb38c859f5b
shane <shane@thegaragelab.com>
2013-06-03 23:13:38 +1000

    Updated tools to generate assets and the basic system assets.

---
+++ tools/mkicon.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+#----------------------------------------------------------------------------
+# 01-Jun-2013 ShaneG
+#
+# This tool is used to generate palette resources from text definitions or
+# from image files.
+#----------------------------------------------------------------------------
+from sys import argv
+from os.path import exists, basename, splitext
+from quickui import *
+
+#----------------------------------------------------------------------------
+# Image processing
+#----------------------------------------------------------------------------
+
+""" Process an RGB file
+"""
+def processRGB(image):
+  print "  Processing RGB graphics file."
+
+""" Process an RGBA file
+"""
+def processRGBA(image):
+  width, height = image.size
+  width = min(width, MAX_IMAGE_WIDTH)
+  height = min(height, MAX_IMAGE_HEIGHT)
+  print "  Processing %i x %i image with alpha channel." % (width, height)
+  bits = ""
+  for y in range(height):
+    line = ""
+    for x in range(width):
+      color = image.getpixel((x, y))
+      if color[3] == 0:
+        line = line + "0"
+      else:
+        line = line + "1"
+    # Pad the line to a full number of bytes
+    while (len(line) % 8) <> 0:
+      line = line + "0"
+    bits = bits + line
+  # All done
+  return width, height, bits
+
+""" Process a monochrome image file
+"""
+def processMono(image):
+  print "  Processing monochrome file."
+
+""" Process a single image file
+"""
+def processImage(filename):
+  # Make sure the file exists
+  if not exists(filename):
+    print "ERROR: The file '%s' does not exist." % filename
+    exit(1)
+  # Open it as an image
+  image = None
+  try:
+    image = Image.open(filename)
+  except:
+    print "ERROR: Could not open '%s' as an image." % filename
+    exit(1)
+  # Start processing the file
+  print "Processing '%s' ..." % filename
+  # Check the size
+  if (image.size[0] > 256) or (image.size[1] > 256):
+    print "  WARNING: Source image is greater than 256 is at least one dimension."
+  # Check the mode and process appropriately
+  if image.mode == "RGB":
+    return processRGB(image)
+  if image.mode == "RGBA":
+    return processRGBA(image)
+  if image.mode == "1":
+    return processMono(image)
+  # If we get here, we failed
+  print "  ERROR: Unsupported graphics mode '%s'." % image.mode
+  exit(1)
+
+#----------------------------------------------------------------------------
+# Main program
+#----------------------------------------------------------------------------
+
+USAGE = """
+Usage:
+
+    %s input-files
+
+Description:
+
+    This utility will generate icon resources from a set of image files. The
+    input files may be in a wide range of formats (jpg, png, bmp, etc) and
+    the generated icon resource will have the same name as the input with the
+    extension changed to '.qco'.
+"""
+
+if __name__ == "__main__":
+  # Have we been given command line arguments ?
+  if len(argv) <= 1:
+    print USAGE % argv[0]
+    exit(1)
+  # Process the input files
+  for filename in argv[1:]:
+    width, height, bits = processImage(filename)
+    outname = splitext(filename)[0] + EXTENSION_ICON
+    print "  Writing resource '%s'" % outname
+    writeIcon(outname, width, height, bits)
--- tools/quickui.py
+++ tools/quickui.py
@@ -7,11 +7,21 @@
 #----------------------------------------------------------------------------
 from struct import pack, unpack
 
+try:
+  from PIL import Image, ImageDraw, ImageFont
+except:
+  print "ERROR: This utility requires the Python Imaging Library."
+  exit(1)
+
 #--- Extensions for various resource file types
 EXTENSION_ICON    = ".qco"
 EXTENSION_IMAGE   = ".qmg"
 EXTENSION_PALETTE = ".qpl"
 EXTENSION_FONT    = ".qfn"
+
+#--- Limits on image sizes
+MAX_IMAGE_WIDTH  = 256
+MAX_IMAGE_HEIGHT = 256
 
 #----------------------------------------------------------------------------
 # Helper functions
@@ -24,6 +34,16 @@ def createColor(red, green, blue):
   result = (((int(green) & 0xFC) << 3) & 0x07E0) | result
   result = (((int(blue) & 0xF8) >> 3)  & 0x001F) | result
   return result
+
+""" Write an image header
+"""
+def writeImageHeader(output, width, height):
+  output.write(pack("BB", width - 1, height - 1))
+
+""" Read an image header
+"""
+def readImageHeader(input):
+  pass
 
 #----------------------------------------------------------------------------
 # Read/Write operations for resources
@@ -47,6 +67,43 @@ def writePalette(filename, palette):
       value = createColor(*palette[index])
     # Write it to the output
     output.write(pack("<H", value))
+  # All done
+  output.close()
+
+""" Read an icon image from a file
+"""
+def readIcon(filename):
+  return None
+
+""" Write an icon resource to a file
+"""
+def writeIcon(filename, width, height, bits):
+  # Open the file
+  output = open(filename, "wb")
+  # Write the header
+  writeImageHeader(output, width, height)
+  # Make sure we have enough bits
+  size = width * height
+  if len(bits) <> size:
+    print "ERROR: Insufficient data provided for icon (Wanted %i, got %i)" % (size, len(bits))
+    exit(1)
+  # Write out the data
+  byte = 0
+  count = 0
+  for ch in bits:
+    # Update the data in the byte
+    byte = byte << 1
+    if ch == "0":
+      byte = byte & 0xFE
+    else:
+      byte = byte | 0x01
+    # Move to the next bit
+    count = count + 1
+    if count == 8:
+      print "%02x" % byte
+      output.write(pack("B", byte & 0xFF))
+      count = 0
+      byte = 0
   # All done
   output.close()
 
