f16b5713d90fc0f0db37cc1ba546a83d21926f32
shane <shane@thegaragelab.com>
2013-06-23 23:54:13 +1000

    Fixed and error with clipping.

--- drivers/common/gfxSetClip.c
+++ drivers/common/gfxSetClip.c
@@ -14,13 +14,13 @@
 /** Set the clipping region
  */
 GFX_RESULT gfx_common_SetClip(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
-  if((x1>0)&&(x1<g_GfxDriver.m_width))
+  if((x1>=0)&&(x1<g_GfxDriver.m_width))
     g_GfxDriver.m_clipX1 = x1;
-  if((x2>0)&&(x2<g_GfxDriver.m_width))
+  if((x2>=0)&&(x2<g_GfxDriver.m_width))
     g_GfxDriver.m_clipX2 = x2;
-  if((y1>0)&&(y1<g_GfxDriver.m_height))
+  if((y1>=0)&&(y1<g_GfxDriver.m_height))
     g_GfxDriver.m_clipY1 = y1;
-  if((y2>0)&&(y2<g_GfxDriver.m_height))
+  if((y2>=0)&&(y2<g_GfxDriver.m_height))
     g_GfxDriver.m_clipY2 = y2;
   // All done
   return GFX_RESULT_OK;
